6f6133e84cf6e05c37f2d1d1ca67423733ccc078
Mrtony94 <35722879+Mrtony94@users.noreply.github.com>
2018-01-30 21:10:19 +0100

    estructura

---
+++ calculadora.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import sys
+
+len_P = len(sys.argv)
+Function_P = {'suma', 'resta', 'mult', 'div'}
+
+def comprobacion():
+
+def suma(num1,num2):
+
+def resta(num1,num2):
+
+def mult(num1,num2):
+
+def div(num1,num2):
+
+
+def operaciones(num1, num2):
+
+
+Function_P = sys.argv[1]
+num1 = float(sys.argv[2])
+num2 = float(sys.argv[3])
+
+comprobacion()
+operaciones(num1,num2)
