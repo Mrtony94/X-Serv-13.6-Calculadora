bc73920c10a8e5ffd2451d794175396cff4027e4
Antonio Andres Perez <tony@MacBook-Pro-de-Antonio.local>
2018-01-30 22:55:24 +0100

    calculadora con errores

--- calculadora.py
+++ calculadora.py
@@ -6,19 +6,30 @@ len_P = len(sys.argv)
 Function_P = {'suma', 'resta', 'mult', 'div'}
 
 def comprobacion():
+    if len_P != 4:
+        sys.exit("Has cometido un error, vuelve a intentarlo")
 
 def suma(num1,num2):
-
+    print(num1, "+", num2, "=", num1 + num2)
 def resta(num1,num2):
-
+    print(num1, "-", num2, "=", num1 - num2)
 def mult(num1,num2):
-
+    print(num1, "*", num2, "=", num1 * num2)
 def div(num1,num2):
-
+    try:
+        print(num1, "/", num2, "=", num1 / num2)
+    except ZeroDivisionError:
+        print ("\nNo se puede dividir entre cero :Sorry\n")
 
 def operaciones(num1, num2):
-
-
+    if Function_P == 'suma':
+        return suma(num1, num2)
+    if Function_P == 'resta':
+        return resta(num1, num2)
+    if Function_P == 'mult':
+        return mult(num1, num2)
+    if Function_P == 'div':
+        return div(num1, num2)
 Function_P = sys.argv[1]
 num1 = float(sys.argv[2])
 num2 = float(sys.argv[3])
