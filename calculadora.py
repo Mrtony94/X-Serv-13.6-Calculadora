93e9f8d10135fa3af581257fb0d10d20c40864a2
Antonio Andres Perez <Tony.mst94@gmail.com>
2018-01-31 23:50:06 +0100

    arreglos

--- calculadora.py
+++ calculadora.py
@@ -3,7 +3,10 @@
 import sys
 
 len_P = len(sys.argv)
-Function_P = {'suma', 'resta', 'mult', 'div'}
+operacion = sys.argv[1]
+num1 = float(sys.argv[2])
+num2 = float(sys.argv[3])
+Function_P = {'sumar', 'restar', 'multiplicar', 'dividir'}
 
 def comprobacion():
     if len_P != 4:
@@ -22,17 +25,16 @@ def div(num1,num2):
         print ("\nNo se puede dividir entre cero :Sorry\n")
 
 def operaciones(num1, num2):
-    if Function_P == 'suma':
+    if operacion == Function_P[0]:
         return suma(num1, num2)
-    if Function_P == 'resta':
-        return resta(num1, num2)
-    if Function_P == 'mult':
-        return mult(num1, num2)
-    if Function_P == 'div':
-        return div(num1, num2)
-Function_P = sys.argv[1]
-num1 = float(sys.argv[2])
-num2 = float(sys.argv[3])
-
+    elif operacion == Function_P[1]:
+         return resta(num1, num2)
+    elif operacion == Function_P[2]:
+         return mult(num1, num2)
+    elif operacion == Function_P[3]:
+         return div(num1, num2)
+    else:
+      sys.exit("Argumento incorrecto, vuelva a intentarlo")
+      
 comprobacion()
 operaciones(num1,num2)
