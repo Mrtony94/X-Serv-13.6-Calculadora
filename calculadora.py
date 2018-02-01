95e46c41dd034af1a8cdbf7769636fd4c4cf746e
Antonio Andres Perez <Tony.mst94@gmail.com>
2018-02-01 13:47:08 +0100

    Version Final

--- calculadora.py
+++ calculadora.py
@@ -6,7 +6,7 @@ len_P = len(sys.argv)
 operacion = sys.argv[1]
 num1 = float(sys.argv[2])
 num2 = float(sys.argv[3])
-Function_P = {'sumar', 'restar', 'multiplicar', 'dividir'}
+Function_P = ['sumar', 'restar', 'multiplicar', 'dividir']
 
 def comprobacion():
     if len_P != 4:
@@ -14,10 +14,13 @@ def comprobacion():
 
 def suma(num1,num2):
     print(num1, "+", num2, "=", num1 + num2)
+
 def resta(num1,num2):
     print(num1, "-", num2, "=", num1 - num2)
+
 def mult(num1,num2):
     print(num1, "*", num2, "=", num1 * num2)
+
 def div(num1,num2):
     try:
         print(num1, "/", num2, "=", num1 / num2)
@@ -35,6 +38,6 @@ def operaciones(num1, num2):
     	return div(num1, num2)
     else:
     	sys.exit("Argumento incorrecto, vuelva a intentarlo")
-    
+    	
 comprobacion()
 operaciones(num1,num2)
